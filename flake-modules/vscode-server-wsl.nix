d4c178026c18224237a95a78b9bb0ac8983e83f7
Yang, Bo <yang-bo@yang-bo.com>
2023-12-06 03:26:16 +0000

    Add executables to `/bin/`

--- flake-modules/vscode-server-wsl.nix
+++ flake-modules/vscode-server-wsl.nix
@@ -8,6 +8,22 @@ topLevel@{ inputs, lib, ... }: {
       # Workaround for https://github.com/nix-community/NixOS-WSL/issues/171
       . ${lib.escapeShellArg nixosModule.config.system.build.setEnvironment}
     '';
+    wsl.extraBin = [
+      # Required by VS Code's Remote WSL extension
+      { src = "${pkgs.coreutils}/bin/dirname"; }
+      { src = "${pkgs.coreutils}/bin/readlink"; }
+      { src = "${pkgs.coreutils}/bin/uname"; }
+      { src = "${pkgs.coreutils}/bin/rm"; }
+      { src = "${pkgs.coreutils}/bin/wc"; }
+      { src = "${pkgs.coreutils}/bin/date"; }
+      { src = "${pkgs.coreutils}/bin/mv"; }
+      { src = "${pkgs.coreutils}/bin/sleep"; }
+      { src = "${pkgs.coreutils}/bin/mkdir"; }
+      { src = "${pkgs.coreutils}/bin/tee"; }
+      { src = "${pkgs.coreutils}/bin/chmod"; }
+      { src = "${pkgs.gnutar}/bin/tar"; }
+      { src = "${pkgs.gzip}/bin/gzip"; }
+    ];
   };
 
 }
