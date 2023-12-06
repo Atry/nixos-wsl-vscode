bc24792df7c7b76642b36c65b3fa6a045dc1c604
Yang, Bo <yang-bo@yang-bo.com>
2023-12-06 03:25:56 +0000

    Add `zlib`, which is required by `~/.vscode-server/bin-cli/*/code-cli`

--- flake-modules/vscode-server.nix
+++ flake-modules/vscode-server.nix
@@ -8,18 +8,23 @@
       type = lib.types.lines;
       description = "the content of `~/.vscode-server/server-env-setup` for each normal user in the NixOS configuration, which would not affect users created from manual `useradd` command.";
     };
-    config.vscodeServer.serverEnvSetup = ''
-      export PATH=$PATH:${lib.strings.makeBinPath [ pkgs.wget ]}
-      export EDITOR="code --wait"
-    '';
-    config.home-manager.users =
-      lib.trivial.pipe nixosModule.config.users.users [
-        (lib.attrsets.filterAttrs (_: userConfig: userConfig.isNormalUser))
-        (lib.attrsets.mapAttrs (userName: userConfig: {
-          home.stateVersion = "23.11";
-          home.file.".vscode-server/server-env-setup".text = nixosModule.config.vscodeServer.serverEnvSetup;
-        }))
+    config = {
+      vscodeServer.serverEnvSetup = ''
+        export PATH=$PATH:${lib.strings.makeBinPath [ pkgs.wget ]}
+        export EDITOR="code --wait"
+      '';
+      home-manager.users =
+        lib.trivial.pipe nixosModule.config.users.users [
+          (lib.attrsets.filterAttrs (_: userConfig: userConfig.isNormalUser))
+          (lib.attrsets.mapAttrs (userName: userConfig: {
+            home.stateVersion = "23.11";
+            home.file.".vscode-server/server-env-setup".text = nixosModule.config.vscodeServer.serverEnvSetup;
+          }))
+        ];
+      programs.nix-ld.libraries = [
+        pkgs.zlib
       ];
+    };
   };
 
 }
