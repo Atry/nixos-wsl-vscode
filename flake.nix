2c0bcdf9cbe7b463b6d324f7a38a52e276800e65
Yang, Bo <yang-bo@yang-bo.com>
2024-04-02 03:44:14 +0000

    Add a substituter for CUDA

--- flake.nix
+++ flake.nix
@@ -65,9 +65,11 @@
 
             nix.settings.extra-substituters = [
               "https://nix-community.cachix.org"
+              "https://cuda-maintainers.cachix.org"
             ];
             nix.settings.extra-trusted-public-keys = [
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
             ];
             nix.settings.auto-optimise-store = true;
             nix.settings.extra-sandbox-paths = lib.mkIf config.wsl.useWindowsDriver [
