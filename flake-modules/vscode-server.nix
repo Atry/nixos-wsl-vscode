4451ae6e28330b23f208e7aacf64e902c5902719
Yang, Bo <yang-bo@yang-bo.com>
2024-09-10 18:37:22 +0000

    Make `home.stateVersion` low priority

--- flake-modules/vscode-server.nix
+++ flake-modules/vscode-server.nix
@@ -4,23 +4,40 @@
       inputs.nix-ml-ops.nixosModules.nixLd
       inputs.home-manager.nixosModules.home-manager
     ];
-    options.vscodeServer.serverEnvSetup = lib.mkOption {
-      type = lib.types.lines;
-      description = "the content of `~/.vscode-server/server-env-setup` for each normal user in the NixOS configuration, which would not affect users created from manual `useradd` command.";
+    options.vscodeServer = {
+      users = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        description = "the list of users who would use VSCode server, by default all the normal users in the NixOS configuration";
+        default = lib.trivial.pipe nixosModule.config.users.users [
+          (lib.attrsets.filterAttrs (_: userConfig: userConfig.isNormalUser))
+          lib.attrNames
+        ];
+      };
+      serverEnvSetup = lib.mkOption {
+        type = lib.types.lines;
+        description = "the content of `~/.vscode-server/server-env-setup`";
+      };
     };
     config = {
       vscodeServer.serverEnvSetup = ''
-        export PATH=$PATH:${lib.strings.makeBinPath [ pkgs.wget ]}
+        export PATH=$PATH:${lib.makeBinPath [ pkgs.wget ]}
         export EDITOR="code --wait"
       '';
-      home-manager.users =
-        lib.trivial.pipe nixosModule.config.users.users [
-          (lib.attrsets.filterAttrs (_: userConfig: userConfig.isNormalUser))
-          (lib.attrsets.mapAttrs (userName: userConfig: {
-            home.stateVersion = "23.11";
-            home.file.".vscode-server/server-env-setup".text = nixosModule.config.vscodeServer.serverEnvSetup;
-          }))
+      home-manager = {
+        sharedModules = [
+          {
+            home.stateVersion = lib.mkDefault "23.11";
+          }
         ];
+        users = lib.pipe nixosModule.config.vscodeServer.users [
+          (builtins.map (name: {
+            inherit name;
+            value.home.file.".vscode-server/server-env-setup".text = nixosModule.config.vscodeServer.serverEnvSetup;
+          }))
+          builtins.listToAttrs
+        ];
+      };
+
       programs.nix-ld.libraries = [
         pkgs.zlib
       ];
